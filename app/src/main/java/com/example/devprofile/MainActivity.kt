d5edd8d34536d46b644c63d7718a6eadc0aaeb6b
salomirc <36732879+salomirc@users.noreply.github.com>
2019-11-19 16:31:28 +0200

    Some images added.

--- app/src/main/java/com/example/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/devprofile/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.devprofile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        var cameraImage: Int = R.drawable.camerabutton
+//        imageView4.setImageResource(cameraImage)
     }
 }
